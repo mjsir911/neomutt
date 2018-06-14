9d5b295043adbc55253201b07a704256be2445b3
Richard Russon <rich@flatcap.org>
2018-06-14 15:23:56 +0100

    listeners

--- mutt_logging.h
+++ mutt_logging.h
@@ -36,5 +36,6 @@ void mutt_log_stop(void);
 int mutt_log_set_level(int level, bool verbose);
 int mutt_log_set_file(const char *file, bool verbose);
 int level_validator(const struct ConfigSet *cs, const struct ConfigDef *cdef, intptr_t value, struct Buffer *err);
+bool mutt_log_listener(const struct ConfigSet *cs, struct HashElem *he, const char *name, enum ConfigEvent ev);
 
 #endif /* _LOGGING2_H */
