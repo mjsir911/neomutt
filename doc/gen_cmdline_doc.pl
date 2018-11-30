01a9510fefed25f2c6070c481423527766b19905
Marco Sirabella <marco@sirabella.org>
2018-11-30 16:04:55 -0500

    Add generator command line args documentation

---
+++ doc/gen_cmdline_doc.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+
+use YAML;
+use strict;
+use warnings;
+
+my @MAP;
+
+my $xml = shift @ARGV;
+
+while (<>) {
+	if (/"options:\\n"/) {
+		read_n_parse_options()
+	}
+}
+
+sub read_n_parse_options {
+	while (my $out = <>) {
+		my ($opt, $arg, $text) = $out =~ /^\s+"\s+(?:((?:\s?(?:-[\w\d-]+))+)\s?(?:<(\w+)>)?\s*)?(.+?)(?:\\n)?"/;
+
+		my $index = defined $MAP[0] ? @MAP : 0;
+		if (defined $opt) {
+			%{ $MAP[$index] } =  (opt => $opt, arg => $arg || "", desc => $text);
+		} else {
+			$MAP[$index - 1]{desc} .= " ";
+			$MAP[$index - 1]{desc} .= $text;
+		}
+
+		if ($out =~ /\)\)\;/) {
+			last;
+		}
+	}
+}
+
+print Dump(\@MAP);
